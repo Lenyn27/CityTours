18431a5c7b49b233b0b151ebb3d6b40909fb32e8
Lenyn27 <1369330@senati.pe>
2022-11-11 22:58:58 -0500

    se actulixo algunas direciones con imagenes etc

--- resources/views/hoteles.blade.php
+++ resources/views/hoteles.blade.php
@@ -14,6 +14,7 @@
 
     </head>
     <body>
+    <!--1-->
         <div class="row row-cols-1 row-cols-md-2 g-4">
         <div class="col">
             <div class="card">
@@ -47,6 +48,45 @@
             <img src="https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/194765160.jpg?k=aa7676fedf9a3d2d7dc67775506e5b463a8a4e2e1e069a86b5ce9bdcab0263e3&o=&hp=1" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Grima Hotel - HUANUCO</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        </div>
+    <!--2-->
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col">
+            <div class="card">
+            <img src="https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/360063650.jpg?k=536c3d299e209d775e374a3d993dc39a0d2b499c5c71672c5c0b00d88d4f896f&o=&hp=1" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">HOTEL ECOLOGICO VILLA JENNIFER - TINGO MARIA</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+            <img src="https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/390668566.jpg?k=18bf2790b1a936ea9f91b0414d68a5d989cd1cb3206fea9ee7cf97fc3cacb51a&o=&hp=1" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Red Lion Hotel Pasco Airport & Conference Center - PASCO </h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+            <img src="https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/247633017.jpg?k=86b3f510554bbeb809816e8480496430abf16bc38d8a5286c72fda9b570532ac&o=&hp=1" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Costa del Sol Wyndham -- PUCALLPA </h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+            <img src="https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/324488211.jpg?k=57f202dafa045db91a5f1a1f7b987c36cdc5e2d4ab33fa9648ecb93eb9778da0&o=&hp=1" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Hotel Hacienda Plaza de Armas - PUNO</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
             </div>
--- resources/views/lugares.blade.php
+++ resources/views/lugares.blade.php
@@ -14,77 +14,103 @@
 
     </head>
     <body>
-        <div class="card" style="width: 18rem;">
-        <img src="https://www.somosperu.org.pe/wp-content/uploads/2016/10/Huanuco-turismo.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
+        <header>
+            
+            <h1>Destinos Recomendados</h1>
 
-        <div class="card" style="width: 18rem;">
-        <img src="https://cdn.www.gob.pe/uploads/document/file/2811023/standard_B92A0083.jpg.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
+        </header>
+        <!--1-->
+            <div class="card" style="width: 18rem;">
+            <img src="https://www.somosperu.org.pe/wp-content/uploads/2016/10/Huanuco-turismo.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Huanuco</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Seleccionar Destino</a>
+            </div>
+            </div>
+        <!--2-->
+            <div class="card" style="width: 18rem;">
+            <img src="https://cdn.www.gob.pe/uploads/document/file/2811023/standard_B92A0083.jpg.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Tingo Maria</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Seleccionar Destino</a>
+            </div>
+            </div>
+        <!--3-->
+            <div class="card" style="width: 18rem;">
+            <img src="https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/390668566.jpg?k=18bf2790b1a936ea9f91b0414d68a5d989cd1cb3206fea9ee7cf97fc3cacb51a&o=&hp=1" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Red Lion Hotel Pasco Airport & Conference Center - CERRO DE PASCO</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Seleccionar Destino</a>
+            </div>
+            </div>
+        <!--4-->
+            <div class="card" style="width: 18rem;">
+            <img src="https://i.ytimg.com/vi/SsuCIwjrfvM/maxresdefault.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Pucallpa</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Seleccionar Destino</a>
+            </div>
+            </div>
+        <!--5-->
+            <div class="card" style="width: 18rem;">
+            <img src="https://i.ytimg.com/vi/l6XQjiZugF0/maxresdefault.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Cusco</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Seleccionar Destino</a>
+            </div>
+            </div>
+        <!--6-->
+            <div class="card" style="width: 18rem;">
+            <img src="https://viajandoalima.com/wp-content/uploads/2016/05/Vista-a%C3%A9rea-de-las-lineas-de-nazca-Peru.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Ica</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Seleccionar Destino</a>
+            </div>
+            </div>
+        <!--7-->
+            <div class="card" style="width: 18rem;">
+            <img src="https://i.ytimg.com/vi/p4pwjLWtx5w/maxresdefault.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Piura</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Seleccionar Destino</a>
+            </div>
+            </div>
+        <!--8-->
+            <div class="card" style="width: 18rem;">
+            <img src="https://www.civa.com.pe/blog/wp-content/uploads/2017/11/Civa-Puno.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Puno</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Seleccionar Destino</a>
+            </div>
+            </div>
 
-        <div class="card" style="width: 18rem;">
-        <img src="https://biblioquinoa.com/bookDB/MM5023/art/portada.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
+        <!--9-->
+            <div class="card" style="width: 18rem;">
+            <img src="https://www.civa.com.pe/blog/wp-content/uploads/2020/01/aniversario-de-tumbes.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Tumbes</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Seleccionar Destino</a>
+            </div>
+            </div>
 
-        <div class="card" style="width: 18rem;">
-        <img src="https://i.ytimg.com/vi/SsuCIwjrfvM/maxresdefault.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-        <img src="https://i.ytimg.com/vi/l6XQjiZugF0/maxresdefault.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-        <img src="https://viajandoalima.com/wp-content/uploads/2016/05/Vista-a%C3%A9rea-de-las-lineas-de-nazca-Peru.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-        <img src="https://i.ytimg.com/vi/p4pwjLWtx5w/maxresdefault.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-        <img src="https://www.civa.com.pe/blog/wp-content/uploads/2017/11/Civa-Puno.png" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-        <img src="https://www.civa.com.pe/blog/wp-content/uploads/2020/01/aniversario-de-tumbes.png" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-        <img src="https://i.ytimg.com/vi/G_yj8hGAhxo/maxresdefault.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-
-
-
+        <!--10-->
+            <div class="card" style="width: 18rem;">
+            <img src="https://i.ytimg.com/vi/G_yj8hGAhxo/maxresdefault.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Ancash</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Seleccionar Destino</a>
+            </div>
+            </div>
+            
     </body>
 </html>
