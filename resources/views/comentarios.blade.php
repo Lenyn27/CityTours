2824d879b5939d51c28120973813cd64019bf610
Lenyn27 <1369330@senati.pe>
2022-11-11 23:05:53 -0500

    se actualizo los comentarios

--- resources/views/comentarios.blade.php
+++ resources/views/comentarios.blade.php
@@ -17,56 +17,56 @@
         <div class="card border-primary mb-3" style="max-width: 18rem;">
         <div class="card-header">Header</div>
         <div class="card-body text-primary">
-            <h5 class="card-title">Primer comnetario</h5>
+            <h5 class="card-title">1</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         </div>
         <div class="card border-secondary mb-3" style="max-width: 18rem;">
         <div class="card-header">Header</div>
         <div class="card-body text-secondary">
-            <h5 class="card-title">Secondary card title</h5>
+            <h5 class="card-title">2</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         </div>
         <div class="card border-success mb-3" style="max-width: 18rem;">
         <div class="card-header">Header</div>
         <div class="card-body text-success">
-            <h5 class="card-title">Success card title</h5>
+            <h5 class="card-title">3</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         </div>
         <div class="card border-danger mb-3" style="max-width: 18rem;">
         <div class="card-header">Header</div>
         <div class="card-body text-danger">
-            <h5 class="card-title">Danger card title</h5>
+            <h5 class="card-title">4</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         </div>
         <div class="card border-warning mb-3" style="max-width: 18rem;">
         <div class="card-header">Header</div>
         <div class="card-body">
-            <h5 class="card-title">Warning card title</h5>
+            <h5 class="card-title">5</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         </div>
         <div class="card border-info mb-3" style="max-width: 18rem;">
         <div class="card-header">Header</div>
         <div class="card-body">
-            <h5 class="card-title">Info card title</h5>
+            <h5 class="card-title">6</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         </div>
         <div class="card border-light mb-3" style="max-width: 18rem;">
         <div class="card-header">Header</div>
         <div class="card-body">
-            <h5 class="card-title">Light card title</h5>
+            <h5 class="card-title">7</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         </div>
         <div class="card border-dark mb-3" style="max-width: 18rem;">
         <div class="card-header">Header</div>
         <div class="card-body text-dark">
-            <h5 class="card-title">Dark card title</h5>
+            <h5 class="card-title">8</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         </div>
